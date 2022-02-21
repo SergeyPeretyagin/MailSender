a13cb0913f802091afc6792c6c559c99d3f9aec8
SergeyPeretyagin <wodnyi33@yandex.ru>
2022-02-21 14:27:41 +0500

    commit 3

---
+++ src/main/java/service/JSONConverter.java
@@ -0,0 +1,45 @@
+package service;
+
+import entity.User;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class JSONConverter {
+
+
+
+    public List<User> parse() throws IOException {
+
+        System.out.println("method parse JSON converter - start");
+        List<User> listUser = new ArrayList<>();
+        JSONParser jsonParser = new JSONParser();
+
+        try {
+
+            JSONObject object = (JSONObject) jsonParser.parse(JsoupParser.parsURL());
+            JSONArray jsonArray = (JSONArray) object.get("Players_data");
+            for (Object o :
+                    jsonArray) {
+                JSONObject jsonObject = (JSONObject) o;
+                String user_name = (String) jsonObject.get("user_name");
+                double spend_time = (Double) jsonObject.get("spend_time");
+                String activities = (String) jsonObject.get("activities");
+                User user = new User(user_name, String.valueOf(spend_time), activities);
+                listUser.add(user);
+                System.out.println(listUser);
+            }
+//            Collections.sort(listUser);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("method parse JSON converter - end");
+        return listUser;
+    }
+}
